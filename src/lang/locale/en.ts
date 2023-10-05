3e2c69a8813f458f00502b0cd4a96b32f3612dc6
Shoujian Zheng <zheng.shoujian@outlook.com>
2023-10-05 23:39:43 +0800

    Use sentence case in UI

--- src/lang/locale/en.ts
+++ src/lang/locale/en.ts
@@ -2,15 +2,15 @@
 
 export default {
   // settings.ts
-  "Access Key": "Access Key",
+  "Access Key": "Access key",
   "Access Key Desc": "You can get key from key management from Qiniu personal center.",
   "Access Key Input": "Enter your access key",
   
-  "Access Secret Key": "Access Secret Key",
+  "Access Secret Key": "Access secret key",
   "Access Secret Key Desc": "You can get key from key management from Qiniu personal center.",
   "Access Secret Key Input": "Enter your access secret key",
   
-  "Bucket Name": "Bucket Name",
+  "Bucket Name": "Bucket name",
   "Bucket Name Desc": "Please indicate the bucket where you would like to upload the image.",
   "Bucket Name Input": "The specific bucket",
 
@@ -18,7 +18,7 @@ export default {
   "Domain Desc": "Go to the Qiniu bucket settings to configure the external link domain name, and enter the domain here.",
   "Domain Input": "The external link domain name of above bucket",
 
-  "Name Prefix": "Name Prefix",
+  "Name Prefix": "Name prefix",
   "Name Prefix Desc": "The uploaded filename's prefix in Qiniu, the uploaded filename will be: {{prefix}}{{timestamp}}.",
   "Name Prefix Input": "The image's prefix",
 };
